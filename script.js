b516badf67c28dc27d3a03da4472d2fb0ebdf3d5
Usike deepika <111295716+usikedeepika@users.noreply.github.com>
2025-05-16 10:00:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
-//your JS code here. If required.
+function checkDays(y) {
+	if( Y%4==0){
+		console.log("366");
+	}
+	else{
+		console.log("365");
+	}
+	
+}
+
+let readline=require('readline');
+
+let  rl=readline.createInterface
+({
+	input:process.stdin,
+	output:process.stdout
+});
+
+rl.questions('Enter a number :',(input)=>{
+
+	const year=parseInt(input)
+			 checkDays(year);
+})
